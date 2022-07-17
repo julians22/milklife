9f3bc1a1855723f4d6a54b48fd17c37b5c584c54
julians22 <dabnerjulian@gmail.com>
2022-07-17 22:03:59 +0700

    update bg style show recipe

--- resources/views/backend/posts/edit.blade.php
+++ resources/views/backend/posts/edit.blade.php
@@ -134,18 +134,45 @@
         const slugable = document.querySelector('[data-slugable]');
 
         document.addEventListener('DOMContentLoaded', function () {
-            tinymce.init({
+            const editor_config = {
+                path_absolute : "/admin/",
+                relative_urls: false,
                 selector: 'textarea#body',
                 height: 500,
                 plugins: ['paste', 'link', 'autoresize', 'media', 'fullpage', 'image', 'imagetools'],
-                toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat| help',
+                toolbar: 'insertfile  undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat| help',
                 content_css: [
                     '//fonts.googleapis.com/css?family=Nunito',
                     '{{ asset('css/backend.css') }}',
                 ],
                 skin: 'oxide',
                 skin_url: '{{ asset('vendor/tinymce/skins/ui/oxide') }}',
-            });
+                file_picker_callback : function(callback, value, meta) {
+                    var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+                    var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+                    var cmsURL = editor_config.path_absolute + 'laravel-filemanager?editor=' + meta.fieldname;
+                    if (meta.filetype == 'image') {
+                        cmsURL = cmsURL + "&type=Images";
+                    } else {
+                        cmsURL = cmsURL + "&type=Files";
+                    }
+
+                    tinyMCE.activeEditor.windowManager.openUrl({
+                        url : cmsURL,
+                        title : 'Filemanager',
+                        width : x * 0.8,
+                        height : y * 0.8,
+                        resizable : "yes",
+                        close_previous : "no",
+                        onMessage: (api, message) => {
+                            callback(message.content);
+                        }
+                    });
+                }
+            };
+
+            tinymce.init(editor_config);
 
             slugable.addEventListener('keyup', delay(function (e) {
                 checkSlug();
--- resources/views/frontend/articles/recipe_show.blade.php
+++ resources/views/frontend/articles/recipe_show.blade.php
@@ -5,7 +5,7 @@
 @section('meta_description', $article->meta_description ?? "")
 
 @section('content')
-    <div class="article-section page" style="background-image: url('{{ asset('img/bg/blue_recipe.jpg') }}');">
+    <div class="article-section page" style="background-image: url('{{ asset('img/bg/blue_recipe.jpg') }}'); background-repeat-y: repeat; background-size: 100%">
         <div class="article-content text-center pb-10">
             <h1 class="article-title text-white font-koara-bold text-4xl my-10 text-center">{{ $article->title }}</h1>
 
